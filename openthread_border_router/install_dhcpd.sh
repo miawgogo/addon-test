856efceafe0d938f7943018b3bb5788e6bbfb372
Ceri Loosley <7449028+miawgogo@users.noreply.github.com>
2025-08-26 13:13:56 +0100

    more fixes

--- openthread_border_router/install_dhcpd.sh
+++ openthread_border_router/install_dhcpd.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
+
+# shellcheck disable=SC1091
+. /usr/src/ot-br-posix/script/_initrc
 # shellcheck disable=SC1091
 . /usr/src/ot-br-posix/script/_dhcpv6_pd_ref
 
-# Override DHCP ref to allow stuff to work
-# its used in the call bellow
-# shellcheck disable=SC2034 
-DHCPV6_PD_REF=1
 # shellcheck disable=SC1073
-dhcpv6_pd_ref_install
+create_dhcpcd_conf_pd
+create_dhcpcd_conf_no_pd
+cp "${DHCP_CONFIG_PD_PATH}" "${DHCP_CONFIG_PATH}"
